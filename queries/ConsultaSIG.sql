22eede6dc154529c237c6af47eb6499a1072e7b3
dev-daviaraujo <dev.daviaraujo@gmail.com>
2025-09-18 15:43:27 -0300

    Retificando organização das pastas

---
+++ queries/ConsultaSIG.sql
@@ -0,0 +1,18 @@
+SELECT 
+    soh.SalesOrderID,
+    soh.OrderDate,
+    soh.TotalDue,
+    sp.Name AS Region,
+    p.Name AS ProductName,
+    sod.OrderQty,
+    sod.UnitPrice
+FROM Sales.SalesOrderHeader AS soh
+INNER JOIN Sales.SalesOrderDetail AS sod
+    ON soh.SalesOrderID = sod.SalesOrderID
+INNER JOIN Production.Product AS p
+    ON sod.ProductID = p.ProductID
+INNER JOIN Person.Address AS a
+    ON soh.ShipToAddressID = a.AddressID
+INNER JOIN Person.StateProvince AS sp
+    ON a.StateProvinceID = sp.StateProvinceID
+ORDER BY soh.OrderDate DESC;
